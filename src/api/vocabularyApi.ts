bc1c8bfabf7136ae12aac01e698116045131b5d3
Justin Wu <juns3700@gmail.com>
2024-11-01 22:12:45 +0800

    feat: refactor

--- src/api/vocabularyApi.ts
+++ src/api/vocabularyApi.ts
@@ -1,13 +1,13 @@
 import axios from 'axios';
-import { VocabularyItem, VocabularyResponse } from '../types/vocabulary';
+import { VocabularyItem } from '../types/vocabulary';
 import { getApiBaseUrl } from './utils';
 
 /* 獲取隨機10個單字 */
 export const fetchVocabulary = async (): Promise<VocabularyItem[]> => {
   const apiBaseUrl = getApiBaseUrl();
-  const response = await axios.get<VocabularyResponse>(`${apiBaseUrl}/api/randomVocabulary`);
-  if (response.data && Array.isArray(response.data.content)) {
-    return response.data.content;
+  const response = await axios.get<VocabularyItem[]>(`${apiBaseUrl}/api/vocabulary/random`);
+  if (response.data && Array.isArray(response.data)) {
+    return response.data;
   } else {
     console.error('Unexpected response format:', response.data);
     return [];

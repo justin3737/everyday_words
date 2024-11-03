eba51b7505345d600203103683ce5fa27a8c2b7a
Justin Wu <juns3700@gmail.com>
2024-11-03 15:57:08 +0800

    feat: 非預期錯誤加入 icon

--- src/components/common/UnexpectedErrorCard.tsx
+++ src/components/common/UnexpectedErrorCard.tsx
@@ -1,4 +1,5 @@
-import { Button, Card, CardBody, Text, VStack } from '@chakra-ui/react';
+import { Button, Card, CardBody, Text, VStack, Icon } from '@chakra-ui/react';
+import { MdError } from 'react-icons/md';
 
 interface UnexpectedErrorCardProps {
   onBack: () => void;
@@ -9,6 +10,7 @@ function UnexpectedErrorCard({ onBack }: UnexpectedErrorCardProps) {
     <Card>
       <CardBody>
         <VStack spacing={4}>
+          <Icon as={MdError} boxSize={8} color="red.500" />
           <Text>發生非預期錯誤，請稍後再試</Text>
           <Button onClick={onBack}>返回上一頁</Button>
         </VStack>
